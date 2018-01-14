3cb51f2e6cba47f2f05386bd4c1ad6a364a332a5
Casey Fleser <cfleser@gmail.com>
2018-01-14 06:41:15 -0600

    Initial Commit

---
+++ Stellar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Stellar
+//
+//  Created by Casey Fleser on 1/14/18.
+//  Copyright © 2018 Quiet Spark. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Stellar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Stellar
+//
+//  Created by Casey Fleser on 1/14/18.
+//  Copyright © 2018 Quiet Spark. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
